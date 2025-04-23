b08fb3ef5664545784e962bb5df68cbe1459ebbd
kalnin_i <ilyukailyukin@gmail.com>
2025-04-23 11:13:23 +0300

    Add project files.

---
+++ OOP lab 11 Form/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace OOP_lab_11_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Показати інформацію про вовка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 225);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(440, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Вовк не тигр, але в цирку не виступає ☝️🐺";
+            this.label2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(574, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Пасхалко";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 253);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(700, 300);
+            this.Name = "Form1";
+            this.Text = "Zoo";
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OOP lab 11 Form/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_lab_11_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Wolf wolf = new Wolf(45.5, 5, 150.0, "Сірий вовк", "Тайга");
+            label1.Text = wolf.GetInfo();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Visible = true;
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            button2.Visible = true;
+        }
+    }
+
+    public class Animal
+    {
+        protected double weight;
+        protected int age;
+        protected double costPerDay;
+
+        public Animal(double weight, int age, double costPerDay)
+        {
+            this.weight = weight;
+            this.age = age;
+            this.costPerDay = costPerDay;
+        }
+
+        public virtual string GetInfo()
+        {
+            return $"Тварина: Вага = {weight} кг, Вік = {age} років, Вартість утримання = {costPerDay} грн/день";
+        }
+    }
+
+    public class Wolf : Animal
+    {
+        private string breed;
+        private string habitat;
+
+        public Wolf(double weight, int age, double costPerDay, string breed, string habitat)
+            : base(weight, age, costPerDay)
+        {
+            this.breed = breed;
+            this.habitat = habitat;
+        }
+
+        public override string GetInfo()
+        {
+            return base.GetInfo() + $"\nПорода вовка: {breed}, Природна локація: {habitat}";
+        }
+    }
+}
